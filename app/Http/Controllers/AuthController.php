24aaea2877d1195aaeff33e537ea8d639ea61a12
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-10-28 19:18:36 -0300

    [feat] implemented token issuance

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+        ]);
+
+        $credentials = request(['email','password']);
+
+        if(!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ],401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->plainTextToken;
+
+        return response()->json([
+            'accessToken' =>$token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\UserController;
+
+Route::post('/tokens', [AuthController::class, 'login']);
 
 Route::apiResource('/users', UserController::class);
 
