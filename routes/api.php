7a156520b08b6958a9406b77f59f0d4ba790d1e7
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-10-29 18:49:05 -0300

    [update] removed auth middleware to register user

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,17 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\UserController;
 
 Route::post('/tokens', [AuthController::class, 'login'])->name('login');
+Route::post('/users', [UserController::class, 'store'])->name('register');
+
+Route::delete('/tokens', [AuthController::class, 'logout'])
+  ->name('logout')
+  ->middleware('auth:sanctum');
+
+Route::apiResource('/users', UserController::class)
+  ->middleware('auth:sanctum')
+  ->except('store');
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
-  Route::delete('/tokens', [AuthController::class, 'logout'])->name('logout');
-
-  Route::apiResource('/users', UserController::class);
   Route::post('/payments/pix', [PaymentController::class, 'pix']);
   Route::post('/payments/simple', [PaymentController::class, 'simple']);
 });
