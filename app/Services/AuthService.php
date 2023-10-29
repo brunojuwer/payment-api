feca908d6c4003346393edb36a8b53ad8e9b63b5
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-10-29 20:00:06 -0300

    [refactor] Created methods to validate credentials and create tokens

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -4,7 +4,10 @@ namespace App\Services;
 
 use App\Exceptions\ForbiddenUserAction;
 use App\Models\User;
+use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
 
 class AuthService 
 {
@@ -13,6 +16,31 @@ class AuthService
     $authenticatedUser = $request->user();
     if($user['id'] !== $authenticatedUser['id']) {
       throw new ForbiddenUserAction;
+    }
+  }
+
+  public static function createToken(Request $request): string
+  {
+    self::validateCredentials($request);
+    $request->user()->tokens()->delete();
+    $user = $request->user();
+    $expirationDateTime = (new DateTime())->modify('+8 hours');
+    $tokenResult = $user->createToken('Personal Access Token', ['*'], $expirationDateTime);
+    return $tokenResult->plainTextToken;
+  }
+
+
+  private static function validateCredentials(Request $request)
+  {
+    $request->validate([
+      'email' => 'required|string|email',
+      'password' => 'required|string',
+    ]);
+
+    $credentials = request(['email','password']);
+
+    if(!Auth::attempt($credentials)) {
+        throw new UnauthorizedException('Bad credentials');
     }
   }
 }
